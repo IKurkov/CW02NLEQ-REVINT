6414455e7c7e676ea83f755989271e9ab8edea82
IKurkov <Ivan.Kurkov@list.ru>
2024-03-03 10:19:29 +0300

    Добавьте файлы проекта.

---
+++ NLEQ.h
@@ -0,0 +1,6 @@
+/* Kurkov Ivan, 22.B05-MM, 03.03.2024 */
+#ifndef NLEQ_H
+#define NLEQ_H
+
+#endif // !NLEQ_H
+
---
+++ REVINT.h
@@ -0,0 +1,6 @@
+/* Kurkov Ivan, 22.B05-MM, 03.03.2024 */
+#ifndef REVINT_H
+#define REVINT_H
+
+#endif // !REVINT_H
+
